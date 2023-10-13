7ce898fc88152c083e94dbf0f927a96ad77a7987
unknown <jesusmolina583@gmail.com>
2023-10-13 22:53:01 +0200

    change sizes gonna cry im crying

--- src/app/components/pageProps/Title.tsx
+++ src/app/components/pageProps/Title.tsx
@@ -5,8 +5,8 @@ export default function Title() {
         <span className="font-extrabold text-blue-600">W</span>ordle
         <span className="font-extrabold text-blue-600">M</span>ania
       </h1>
-      <p className="text-center pt-8 pb-1 text-xl ">Wait one day? nah</p>
-      <p className="text-center  text-lg text-blue-400">
+      <p className="text-center pt-8 pb-1 text-lg ">Wait one day? nah</p>
+      <p className="text-center text-blue-400">
         Words with more/less than 5 characters!
       </p>
     </>
