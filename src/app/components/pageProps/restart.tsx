0652436c7923d9885d9110368be5b236a45bc00b
unknown <jesusmolina583@gmail.com>
2023-10-14 17:34:01 +0200

    final commit

--- src/app/components/pageProps/restart.tsx
+++ src/app/components/pageProps/restart.tsx
@@ -24,8 +24,8 @@ export default function Restart({
     <>
       <button
         className={`flex justify-center m-auto -mt-24 lg:-mt-52 border-2 
-        px-6 py-2 rounded-md  text-white font-semibold
-        hover:scale-105 hover:text-lg transition-all ${btnColor}`}
+        px-6 py-2 rounded-md text-sm text-white font-semibold
+        hover:scale-105 hover:text-base transition-all ${btnColor}`}
         onClick={() => generateWord()}
       >
         {!isLoaded ? (
