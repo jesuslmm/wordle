45eca8189f856d1958d6296001ec75c9cd35bd2f
unknown <jesusmolina583@gmail.com>
2023-10-14 17:27:16 +0200

    redo the loading button

--- src/app/components/pageProps/restart.tsx
+++ src/app/components/pageProps/restart.tsx
@@ -25,13 +25,13 @@ export default function Restart({
       <button
         className={`flex justify-center m-auto -mt-24 lg:-mt-52 border-2 
         px-6 py-2 rounded-md  text-white font-semibold
-        hover:scale-105 hover:text-xl transition-all ${btnColor}`}
+        hover:scale-105 hover:text-lg transition-all ${btnColor}`}
         onClick={() => generateWord()}
       >
         {!isLoaded ? (
           <>
             <svg
-              className=" -ml-8 animate-spin"
+              className="-ml-4 animate-spin"
               fill="none"
               height="22"
               viewBox="0 0 48 48"
